132bde12c55b51e7ff53a004e8318a35f91fc6ed
Doğan Kocadayılar <dogankocadayilar@gmail.com>
2021-10-30 13:56:33 +0300

    Add Level_1

--- Assets/Scripts/CameraFallow.cs
+++ Assets/Scripts/CameraFallow.cs
@@ -7,13 +7,23 @@ public class CameraFallow : MonoBehaviour
     [SerializeField] float dampTime = 0.15f;
     [SerializeField] Transform target;
     private Vector3 velocity = Vector3.zero;
+    float yPos;
 
     // Update is called once per frame
     void Update()
     {
-        if (target.position.x > 0)
+        if (target.position.x > 0 || target.position.y < -5)
         {
-            Vector3 destination = new Vector3(target.position.x, transform.position.y, transform.position.z);
+            if(target.position.y < -5.5f)
+            {
+                yPos = -15;
+            }
+            else
+            {
+                yPos = 0;
+            }
+
+            Vector3 destination = new Vector3(target.position.x, yPos, transform.position.z);
             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
         }
 
